6699568b92840de9c3d7ad08aac833005ad329a3
MuhammadUsama021 <muhammadusama008233@gmail.com>
2021-10-07 18:52:58 +0500

    Theory_Assignment_1 Uploaded

---
+++ Theory_Assignment_1/app/src/main/java/com/usamacreation/theory_assignment_1/Constraint_Layout.kt
@@ -0,0 +1,11 @@
+package com.usamacreation.theory_assignment_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Constraint_Layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint__layout)
+    }
+}
---
+++ Theory_Assignment_1/app/src/main/java/com/usamacreation/theory_assignment_1/Relative_Layout.kt
@@ -0,0 +1,11 @@
+package com.usamacreation.theory_assignment_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Relative_Layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative__layout)
+    }
+}
